8208961cb077266a9a7724fc7872b9e8108f8628
Addison <S10222525@connect.np.edu.sg>
2022-11-21 21:56:30 +0800

    chore: code documentation

--- lib/logic.dart
+++ lib/logic.dart
@@ -7,28 +7,33 @@ import 'dart:math'; // for random number
 TextEditingController answerController = new TextEditingController();
 
 // Variables
-String api_key =
-    "live_b8VFtq3DpPQmPOh271GEJryIOYXv6yhQFiAAzcw1GlRwbcIKs2ShAV0AOQkRvzX5";
+String api_key = ""; // Define API Key
 
-List catBreeds = [];
-String answer = "";
-String image = "https://cdn2.thecatapi.com/images/NZpO4pU56M.jpg";
+List catBreeds = []; // List of all the cat breeds (in API)
+String answer = ""; // Answer for the quiz (will change randomly)
+String image =
+    "https://cdn2.thecatapi.com/images/NZpO4pU56M.jpg"; // Image URL for the Image (NetworkImage) Widget
 
 void initGame() async {
   // --- Get Cat Breeds ----
   String breedURL = "https://api.thecatapi.com/v1/breeds";
   var res = await http.get(Uri.parse(breedURL),
       headers: {"x-api-key": api_key, "Access-Control-Allow-Origin": "*"});
+
   // Convert to JSON
   var data = jsonDecode(res.body);
+
+  // Add cat breeds to list
   for (var breed in data) {
     catBreeds.add(breed["id"]);
   }
+
+  // On first load, get a new cat :D
   getCat();
 }
 
 void getCat() async {
-  // Randomize
+  // Randomize Cat Breed -> for next image & answer
   int random = new Random().nextInt(catBreeds.length);
   String breed = catBreeds[random];
   // Get Cat Image
@@ -36,24 +41,23 @@ void getCat() async {
       "https://api.thecatapi.com/v1/images/search?breed_ids=$breed";
   var res = await http.get(Uri.parse(imageURL),
       headers: {"x-api-key": api_key, "Access-Control-Allow-Origin": "*"});
-  // Convert to JSON
+
+  // Convert response to JSON
   var data = jsonDecode(res.body);
 
-  String previous_image = image;
+  // Update Image & Answer
   image = data[0]["url"];
   answer = data[0]["breeds"][0]["name"];
-  print(image);
 }
 
 bool checkAnswer() {
-  // Get Textbox Value
-  String user_answer = answerController.text;
+  String user_answer = answerController.text; // Get Textbox Value
   print("User Answer: $user_answer | Answer: $answer");
 
   if (user_answer.toLowerCase() == answer.toLowerCase()) {
     print("Correct");
-    getCat();
-    answerController.clear();
+    getCat(); // Get a new cat (image & answer)
+    answerController.clear(); // Clear TextField Widget
     return true;
   }
   print("Incorrect");
